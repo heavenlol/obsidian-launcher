5a6cd1237352d8f140c8385591cbff630ab9636f
deb <tows9888@gmail.com>
2026-06-10 23:46:43 -0500

    Update MineButton.java
    Signed-off-by: deb <tows9888@gmail.com>

--- app_pojavlauncher/src/main/java/com/kdt/mcgui/MineButton.java
+++ app_pojavlauncher/src/main/java/com/kdt/mcgui/MineButton.java
@@ -1,28 +1,33 @@
 package com.kdt.mcgui;
 
 import android.content.*;
-import android.graphics.*;
+import android.graphics.drawable.GradientDrawable;
 import android.util.*;
 
+import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
 
 import net.kdt.pojavlaunch.R;
 
 public class MineButton extends androidx.appcompat.widget.AppCompatButton {
-	
-	public MineButton(Context ctx) {
-		this(ctx, null);
-	}
-	
-	public MineButton(Context ctx, AttributeSet attrs) {
-		super(ctx, attrs);
-		init();
-	}
 
-	public void init() {
-		setTypeface(ResourcesCompat.getFont(getContext(), R.font.noto_sans_bold));
-		setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.mine_button_background, null));
-		setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimensionPixelSize(R.dimen._13ssp));
-	}
+    public MineButton(Context ctx) {
+        this(ctx, null);
+    }
 
+    public MineButton(Context ctx, AttributeSet attrs) {
+        super(ctx, attrs);
+        init();
+    }
+
+    public void init() {
+        setTypeface(ResourcesCompat.getFont(getContext(), R.font.noto_sans_bold));
+        setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimensionPixelSize(R.dimen._13ssp));
+
+        GradientDrawable bg = new GradientDrawable();
+        bg.setShape(GradientDrawable.RECTANGLE);
+        bg.setColor(ContextCompat.getColor(getContext(), R.color.minebutton_color));
+        bg.setCornerRadius(8f);
+        setBackground(bg);
+    }
 }
